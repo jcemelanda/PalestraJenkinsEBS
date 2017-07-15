1641a3045e8142ac211dc6021b75ce3eb9c1d619
Julio Melanda <jcemelanda@julio.melanda>
2017-07-15 03:52:00 -0300

    Fixes allowed hosts

--- palestra/settings.py
+++ palestra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%r+z)9kzy)w%@y)w8o4bt!&j3%lhsceg^p5(^i9xyqn89ok0r@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'palestra-ebs.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
